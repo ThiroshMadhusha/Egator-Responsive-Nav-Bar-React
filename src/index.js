bf6fdc2e0d685620998fdfb02fb058a3e0e7fd64
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-28 10:39:24 +0530

    create react app

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(<App />);
